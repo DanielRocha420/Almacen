781701bc70d35a00d70e127d8e178846c5b7f253
DanielRocha420 <danielhernadezrocha9@gmail.com>
2026-09-01 11:16:36 -0600

    Proyecto Almacen Iniciado

---
+++ src/test/java/com/daniel/almacen/AlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
